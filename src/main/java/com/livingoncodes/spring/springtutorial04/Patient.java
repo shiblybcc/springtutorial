740afa936cf4f744da998dd7abe3b6f91e437f6f
bcc <shiblyxp@gmail.com>
2018-03-07 20:58:37 +0100

    done spring07

--- src/main/java/com/livingoncodes/spring/springtutorial04/Patient.java
+++ src/main/java/com/livingoncodes/spring/springtutorial04/Patient.java
@@ -5,12 +5,22 @@ public class Patient {
 	private int id;
 	private String name;
 	
+	private int nationalId;
+	
 	public Patient() {		
 	}	
 	
 	public Patient(int id, String name) {
 		this.id = id;
 		this.name = name;
+	}		
+
+	public int getNationalId() {
+		return nationalId;
+	}
+
+	public void setNationalId(int nationalId) {
+		this.nationalId = nationalId;
 	}
 
 	public int getId() {
@@ -27,11 +37,11 @@ public class Patient {
 
 	public void setName(String name) {
 		this.name = name;
-	}
+	}	
 
 	@Override
 	public String toString() {
-		return "Patient [id=" + id + ", name=" + name + "]";
+		return "Patient [id=" + id + ", name=" + name + ", nationalId=" + nationalId + "]";
 	}
 
 	public void speak() {
